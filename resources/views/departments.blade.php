69fab06daa4778adb3bbc5d8240f5dc30b161bc0
Piccolo <kabiryusufbashir@gmail.com>
2022-12-18 19:57:57 +0100

    Create Department page

---
+++ resources/views/departments.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.template')
+
+@section('page-title')
+    Departments - Police HMO - Promoting Excellent Healthcare 
+@endsection
+
+@section('body-content')
+    <!-- Body Contents  -->
+    <div class="pt-8 pb-5 text-justify relative top-10">
+        <!-- Board  -->
+        <div class="bg-gray-200 mt-4 py-16">
+            <div class="text-center text-xl lg:text-xl py-4">Police Health Maintenance Limited</div>
+            <div class="text-center text-2xl lg:text-5xl font-semibold py-4 mb-5">Our Departments</div>
+            
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center my-6">
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Medical Services/QA</div>
+                    <div class="text-sm py-1">Medical services department is one of the operational departments of the Police Health Maintenance Limited that involves both Medical and Non-medical activities that centers on the enrollee management, Health care Provider management, Monitoring and Evaluation/Quality Assurance and encounter management. The desires are that, the enrollees of Police Health Maintenance Limited received quality healthcare services in conjunction with the network of both public and private health care providers accredited under National Health Insurance Scheme</div>
+                    <div class="text-sm py-1">In order to have proper supervision and delineation of responsibilities for all the staff in the department, the department is divided into units as follows:</div>
+                    <div class="text-lg py-1 font-semibold">Enrollee Liaison/ Management</div>
+                    <div class="text-sm py-1">To build and increase enrollee confidence, satisfaction, safety and to improve quality healthcare services provided to our enrollees, we under take health education and promotion by developing enrollee health education pamphlets and management strategies for pre, intra and post admission care through follow up either by direct visit to Health care provider or phone calls. The department ensures PHML enrollees are aware of the beneficiary package and their rights as a beneficiary of National Health Insurance Scheme through and intensive enrollee education. In ensuring enrollees satisfaction and safety, their enquiries/complaints are given prompt and desirous attention. As part of ensuring that our enrollees receive comprehensive, effective, efficient and quality health care, we ensure in collaboration with our Call Centre, they get pre-authorization code through appropriate referral protocol to specialist and performing of enrollees case files Medical Auditing</div>
+                    <div class="text-lg py-1 font-semibold">Health Care Provider Liaison/ Management</div>
+                    <div class="text-sm py-1">In ensuring the organization maintain trusted and cordial relationships with all providers under the PHML network, this unit created a WhatsApp group with all Provider Desk Officers and the State Coordinators as medium of communication to ensure the organization has effective and efficient communication with the providers. The department as well, organize HCPs versus PHMO interactive forum every quarter through which HCPs are educated on NHIS benefit package, referral protocols, medical bill preparation and submission and ensuring all contractual agreements with them are respected. This medium provides opportunity for the department to collate and attend to the HCPs enquiries/complaints.</div>
+                    <div class="text-lg py-1 font-semibold">Monitoring & Evaluation/Quality Assurance</div>
+                    <div class="text-sm py-1">The HCPs under PHML are continuously being monitored to ensure that high standard of health care services is provided to the enrollees. In order to achieve this, the department undergo monitoring and evaluation of the facility’s human and material resources by ensuring the availability of qualified personnel, equipment/tools and standard operational procedures and NHIS guidelines are followed at all the service points, and the services have the desired effect on the beneficiaries.</div>
+                    <div class="text-lg py-1 font-semibold">Enrollee Encounter Data Management</div>
+                    <div class="text-sm py-1">This unit collates data, enters and analyze all enrollee visits and encounter with the HCPs under PHML network in a bid to determine health care service utilization by our enrollees, disease trend distribution in various states of the country. This will help PHML in developing her health policy and intervention strategies and as well as generation of epidemiological data for the country</div>
+                    <div class="text-sm py-1">In addition to above responsibilities, the department collaborates with other departments to perform other functions and any other responsibility given by the management when necessary with the aim of achieving the Organizational goal and objectives.</div>
+                    <div class="text-sm py-1">The reports generated from these activities are submitted to NHIS every quarter in compliance with the NHIS Operation guidelines.</div>
+                </div>
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Administration & HR</div>
+                    <div class="text-lg py-1 font-semibold">Administration</div>
+                    <div class="text-sm py-1">is the backbone of an organization. The department provides administrative and technical support in the area of human resources in addition to serving as a link between the organisation’s (Police Health Maintenance Limited) various departments and ensures the smooth flow of information within the organisation. Thus, without an effective administrative department, Police Health Maintenance Limited would not run professionally and smoothly as it is the case today. Its activities includes but not limited to the execution of the administrative functions such as preparing budgets, policies, contracts, maintenance of company facilities, provision of working implement for efficient and conducive day to day running of a result oriented work place, long range planning, and keeping inventories of equipment and managing the company’s physical assets and utilities all within the Head office and the 36 State offices.</div>
+                    <div class="text-sm py-1">The department consist of five sub unit for effective operation. This you can find below</div>
+                    <div class="text-sm py-1 px-4">
+                        <div>1) Customer Care Unit</div>
+                        <div>2) Facility Management Unit</div>
+                        <div>3) Procurement Unit</div>
+                        <div>4) Inventory Management</div>
+                        <div>5) Transport Unit</div>
+                    </div>
+                    <div class="text-lg py-1 font-semibold">Human Resources</div>
+                    <div class="text-sm py-1">department in Police Health Maintenance Limited is tasked with the responsibility of hiring qualified persons with requisite qualification and possibly wealth of experience relevant to the working process of the organization, keeping them engaged, Leave Management and supporting their growth and development within the Head Office and 36 state offices around the country. Human Resource leads Recruiting and Employee Onboarding and as well assists Managers and Team Leaders all geared towards improving the processes and practices within the organizations as it concerns staff development. The department administers Human resource-related documentation, such as contract of employment. Ensure the relevant Human resource database is up to date, accurate and complies with legislation or in accordance with the service manual and extant laws.</div>
+                    <div class="text-sm py-1">It is worthy to note that while administration is in charge of managing the company’s physical asset and utilities, making the decisions, and overseeing the service details of the company, Human Resource deals with Human Beings (Employees) and in addition deals with the support functions related to those Employees. It is responsible for the management of the day-to-day operations of all personnel administrative matters guided by the implemented Human resource procedures and policies.</div>
+                </div>
+            </div>
+
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center my-6">
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Finance & Accounts</div>
+                    <div class="text-sm py-1">The Finance and Accounts Department is manned by a team of highly motivated and responsible staff with a unique blend of experiences drawn from different sectors of the economy.</div>
+                    <div class="text-sm py-1">Headed by a Fellow of the Institute of Chartered Accountants of Nigeria and assisted by equally competent and proactive staff with experiences in Health Financing, Auditing and Financial Services.</div>
+                    <div class="text-sm py-1">The Finance and Accounts Department is responsible for the following:</div>
+                    <div class="text-lg py-1 font-semibold">Statutory Payments</div>
+                    <div class="text-sm py-1 px-4">
+                        <div>- Capitation Payment</div>
+                        <div>- Fee for Service</div>
+                        <div>- Tertiary Assistance</div>
+                        <div>- Others</div>
+                    </div>
+                    <div class="text-lg py-1 font-semibold">Employee/General Payments</div>
+                    <div class="text-sm py-1 px-4">
+                        <div>- Salary Payments</div>
+                        <div>- Tax Deduction & Remittance</div>
+                        <div>- Pension Deduction & Remittance</div>
+                        <div>- Accounts Payables/Receivables</div>
+                    </div>
+                    <div class="text-lg py-1 font-semibold">Management Information System</div>
+                    <div class="text-sm py-1 px-4">
+                        <div>- Annual Accounts & Financials</div>
+                        <div>- Quarterly Management Reports</div>
+                        <div>- Annual Budget</div>
+                        <div>- Etc</div>
+                    </div>
+                </div>
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">ICT</div>
+                    <div class="text-sm py-1">This department is responsible for developing IT strategies that support the organization’s objectives, developing and supporting the network to support effective communication and collaboration, IT infrastructure security, ensuring that the IT infrastructure remains operational and offering support to all PHML staff to enable them function more effectively. The department also generates operational reports to aid critical business decisions.</div>
+                    <div class="text-sm py-1">Therefore, the Information & Communications Technology Department is the backbone of the entire PHML operations and comprises the following organisational units:</div>
+                    <div class="text-sm py-1 px-4">
+                        <div>1) Data Management & Analysis</div>
+                        <div>2) Support & Maintenance Services</div>
+                        <div>3) IT Infrastructure & Services</div>
+                        <div>4) Applications Development & Administration</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center my-6">
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Business Development & Enlightenment</div>
+                    <div class="text-sm py-1">This department is tasked with the mandate to ensuring that Officers and Men of the Nigerian Police Fore, Police retirees and their respective families are adequately enrolled and receiving satisfactory medical services under the National Health Insurance Scheme (NHIS). The department among others engages in:</div>
+                    <div class="text-lg py-1 font-semibold">Business Development</div>
+                    <div class="text-sm py-1">Identifying new potentials for ultimate enrolment in the Scheme</div>
+                    <div class="text-sm py-1">Prospecting, development and engaging in other means of activities that generate interest from potential enrollees.</div>
+                    <div class="text-lg py-1 font-semibold">Enlightenment</div>
+                    <div class="text-sm py-1">Periodic sensitization exercises in Commands, Divisions, and Barracks etc. nationwide to properly update existing and prospective enrollees on the workings of the Scheme.</div>
+                    <div class="text-sm py-1">Identifying new potentials for ultimate enrolment.</div>
+                    <div class="text-sm py-1">Review and build good relationship with new and existing enrollees.</div>
+                    <div class="text-lg py-1 font-semibold">Retirees Scheme</div>
+                    <div class="text-sm py-1">Registration, sensitization and maintenance of the health insurance of all Nigeria Police Retirees registered into the Scheme.</div>
+                </div>
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Corporate Affairs</div>
+                    <div class="text-sm py-1">The corporate Affairs unit is saddled with the responsibility of tracking and keeping in touch with information as it concerns PHML. This includes generating and disseminating same when required. The value the public puts on the PHML is determined by their perception. Basically, the corporate affairs unit is the image maker of PHML. It is therefore the duty of the corporate Affairs unit to ensure that the public has a good perception of PHML. These can be achieved through effective communication to staff, public, as well as stakeholders in the industry (hospitals, enrolees, NHIS etc.). The means includes public relations, government relations, advocacy, corporate social responsibility and so on.</div>
+                </div>
+            </div>
+
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center my-6">
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Internal Control & Audit</div>
+                    <div class="text-sm py-1">The Internal Control and Audit Department is The Board's functional and Management's administrative institution positioned to improve the effectiveness of business governance.</div>
+                    <div class="text-sm py-1">We demonstrate commitment to professionalism and objectivity, in discharging our duties which are not limited to:</div>
+                    <div class="text-sm py-1 px-4">
+                        <div>1) Establishing and sustaining a working relationship with other departments with a view to driving compliance to organisational policies.</div>
+                        <div>2) Review of organisation's Information and business processes for effectiveness, with the aim of making recommendations for improvement where there are opportunities to do so.</div>
+                        <div>3) Assessment of risk and its management procedures, thereby controlling risk appetite and defining risk limits.</div>
+                        <div>4) Evaluating the means of safeguarding assets and as appropriate, their verification.</div>
+                        <div>5) Ensuring that the organisation is complying with regulatory and statutory provisions to avoid the sledgehammer of the law.</div>
+                        <div>6) Promoting responsible leadership, accountability and commitment to a competent/motivated workforce.</div>
+                        <div>7) Evaluating management controls for adequacy and effectiveness.</div>
+                    </div>
+                </div>
+                <div class="text-justify bg-white p-4">
+                    <div class="text-3xl py-1 font-semibold">Operations</div>
+                    <div class="text-sm py-1">The department is responsible for the creation of the highest level of efficiency possible within the organization by ensuring and increasing the effectiveness and efficiency of support services (HR, IT and finance), through improvements to each function as well as coordination and communication between support and business functions, creating and monitoring performance of systems, improving the operational system, processes and policies in support of the organization’s mission, playing a significant role in long term planning, including initiatives geared towards operational excellence and in addition, the department consist of the following unit and human resources.</div>
+                    <div class="text-lg py-1 font-semibold">Claims Unit</div>
+                    <div class="text-sm py-1">The Claims Unit is responsible for all processes concerned with the medical and financial adjudication of fee-for-service claims presented by health care providers for payment. This unit is supported by a robust claims software, which has significantly improve the efficiency of the unit.</div>
+                    <div class="text-lg py-1 font-semibold">Call Center</div>
+                    <div class="text-sm py-1">The Call Centre Unit acts as the gatekeeper of the organization as it is the first line responder to all complaints and clarifications emanating from both enrollees and providers. The Unit is also responsible for the generation and dissemination of pre-authorization codes to providers for secondary level management.</div>
+                    <div class="text-lg py-1 font-semibold">Claims Resolution Unit</div>
+                    <div class="text-sm py-1">The unit is responsible for all process relating to the receipt, collation and resolution of complaints emanating from Healthcare Providers relating to the following: Non-payment of fee-for-service claims, short payment of fee for service claims, non-submission of payment advice to HCFs after payment of fee-for-service claims and non-submission of payment analysis to HCPs after payment of fee-for-service claims. It’s also the responsibility of the unit to inform the regulator on the status of resolution.</div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+@endsection
