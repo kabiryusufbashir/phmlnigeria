7c28f809dc7e6ea4b6d2a04f1f927d58b353a75d
Piccolo <kabiryusufbashir@gmail.com>
2023-01-07 11:42:16 +0100

    Add Mds profile

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -11,45 +11,57 @@
         <div class="bg-white mt-4 py-16">
             <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center">
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/chairman.jpeg') }}" alt="IGP Alkali Baba Usman, psc (+), NPM, fdc Photo">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/chairman.jpeg') }}" alt="IGP Alkali Baba Usman, psc (+), NPM, fdc Photo">
                     <div class="text-lg py-1 font-semibold">IGP Alkali Baba Usman, psc (+), NPM, fdc</div>
                     <div class="text-sm py-1">Chairman, Board of Directors</div>
                     <div class="text-xs py-1">The Inspector General of Police</div>
                 </div>
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/vice_chairman.jpeg') }}" alt="DIG Sanusi M. Lemu mni">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/vice_chairman.jpeg') }}" alt="DIG Sanusi M. Lemu mni">
                     <div class="text-lg py-1 font-semibold">DIG Sanusi M. Lemu mni</div>
                     <div class="text-sm py-1">Vice-Chairman of the Board</div>
                     <div class="text-xs py-1">DIG (DFA)</div>
                 </div>
             </div>
-            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-3 gap-6 items-center my-6">
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center my-6">
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/no_photo.jpg') }}" alt="DIG Mohammed Aminu Yesufu Photo">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/no_photo.jpg') }}" alt="DIG Mohammed Aminu Yesufu Photo">
                     <div class="text-lg py-1 font-semibold">DIG Mohammed Aminu Yesufu</div>
                     <div class="text-sm py-1">(Retd.)</div>
-                    <div class="text-xs py-1">Board Member</div>
+                    <div class="text-xs py-1">Director</div>
                 </div>
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/board_member_001.jpeg') }}" alt="AIG Dr. (Mrs.) Adenike Abuwa Photo">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/board_member_001.jpeg') }}" alt="AIG Dr. (Mrs.) Adenike Abuwa Photo">
                     <div class="text-lg py-1 font-semibold">AIG Dr. (Mrs.) Adenike Abuwa</div>
                     <div class="text-sm py-1">(Retd.)</div>
-                    <div class="text-xs py-1">Board Member</div>
+                    <div class="text-xs py-1">Director</div>
+                </div>
+            </div>
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center my-6">
+                <div class="text-center">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/no_photo.jpg') }}" alt="CP Dr. Emmanuel Garba">
+                    <div class="text-lg py-1 font-semibold">CP Dr. Emmanuel Garba</div>
+                    <div class="text-xs py-1">Director (FMO)</div>
                 </div>
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/independent_director.jpg') }}" alt="DIG Sanusi M. Lemu mni">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/no_photo.jpg') }}" alt="CP Isyaku Mohammed">
+                    <div class="text-lg py-1 font-semibold">CP Isyaku Mohammed</div>
+                    <div class="text-xs py-1">Director (PAB)</div>
+                </div>
+            </div>
+            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-3 gap-6 items-center">
+                <div class="text-center">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/independent_director.jpg') }}" alt="DIG Sanusi M. Lemu mni">
                     <div class="text-lg py-1 font-semibold">Dr. Otive Igbuzor</div>
                     <div class="text-xs py-1">Independent Director</div>
                 </div>
-            </div>
-            <div class="lg:px-24 px-8 mx-auto pb-8 lg:grid grid-cols-2 gap-6 items-center">
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/ceo.jpeg') }}" alt="Dr. Yusuf Kalli Gazali Photo">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/ceo.jpeg') }}" alt="Dr. Yusuf Kalli Gazali Photo">
                     <div class="text-lg py-1 font-semibold">Dr. Yusuf Kalli Gazali</div>
                     <div class="text-xs py-1">Managing Director/C.E.O</div>
                 </div>
                 <div class="text-center">
-                    <img class="w-64 mx-auto rounded-full" src="{{ asset('images/secretary.jpg') }}" alt="Barr. Suleiman Idris Photo">
+                    <img class="w-64 mx-auto rounded-full border border-green-400 border-2" src="{{ asset('images/secretary.jpg') }}" alt="Barr. Suleiman Idris Photo">
                     <div class="text-lg py-1 font-semibold">Barr. Suleiman Idris</div>
                     <div class="text-xs py-1">Secretary of The Board</div>
                 </div>
--- resources/views/mds.blade.php
+++ resources/views/mds.blade.php
@@ -13,19 +13,33 @@
             <div class="text-center text-2xl lg:text-5xl font-semibold py-4 mb-5">MDs</div>
             
             <div class="lg:px-24 px-8 mx-auto pb-8 items-center">
-                <div class="my-4">
-                    <div class="text-center">
-                        <img class="border border-green-600 w-64 mx-auto mt-4" src="{{ asset('images/md-001.png') }}" alt="Dr. Yusuf Kalli Gazali Photo">
+                <div class="my-4 items-center lg:grid grid-cols-2 bg-gray-100 lg:w-3/4 mx-auto">
+                    <div class="py-5 px-8">
+                        <h1 class="lg:text-2xl text-xl font-semibold">PROFILE OF DR. YUSUF KALLI GAZALI</h1>
+                        <p class="my-3">
+                            Dr. Yusuf Kalli Gazali is the Managing Director/CEO who over sees all the aspects of the company's operation
+                        </p>
+                        <p class="my-3">
+                            A graduate from the University of Maiduguri, Dr. Gazali has over 13 years of experience in medical practice in diverse fields like Pathology, Microbiology, Otorhinolaryngology and family medicine. Outside medical practice, he also has vast experience in project management.
+                        </p>
                     </div>
-                    <div class="bg-gray-500 mx-auto text-center w-64 text-white py-3">
-                        <div class="text-lg">Dr Yusuf Kalli Gazali</div> 
-                        <div class="text-sm">MD/CEO POLICE HEALTH MAINTENANCE</div> 
-                        <div class="text-xs">October 2022 - Present</div> 
+                    <div class="text-center float-right">
+                        <img class="border border-2 border-green-600 lg:w-2/3 float-right" src="{{ asset('images/md-001.png') }}" alt="Dr. Yusuf Kalli Gazali Photo">
                     </div>
                 </div>
             </div>
             <div class="lg:px-24 px-8 mx-auto pb-8 items-center">
                 <div class="lg:flex justify-center">
+                    <div class="border border-green-600 my-4">
+                        <div class="text-center">
+                            <img class="w-64 mx-auto mt-4" src="{{ asset('images/md-001.png') }}" alt="Dr. Yusuf Kalli Gazali Photo">
+                        </div>
+                        <div class="bg-gray-500 mx-auto text-center w-64 text-white py-3">
+                            <div class="text-lg">Dr Yusuf Kalli Gazali</div> 
+                            <div class="text-sm">MD/CEO POLICE HEALTH MAINTENANCE</div> 
+                            <div class="text-xs">September 2022 - Present</div> 
+                        </div>
+                    </div>
                     <div class="border border-green-600 my-4">
                         <div class="text-center">
                             <img class="w-64 mx-auto mt-4" src="{{ asset('images/md-002.png') }}" alt="Dr Ahmad Garba Yakasai">
@@ -53,7 +67,7 @@
                         <div class="bg-gray-500 mx-auto text-center w-64 text-white py-3">
                             <div class="text-lg">AIG (RTD) Alex Emeka Okeke</div> 
                             <div class="text-sm">MD/CEO POLICE HEALTH MAINTENANCE</div> 
-                            <div class="text-xs">January 2017 - December 2017</div> 
+                            <div class="text-xs">June 2017 - December 2017</div> 
                         </div>
                     </div>
                     <div class="border border-green-600 my-4">
